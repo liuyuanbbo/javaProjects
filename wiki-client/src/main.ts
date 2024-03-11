33bd297f2f8c60f83cad380ea6d704533a4abc3a
Zmz <Zmz@gmail.com>
2024-03-11 15:42:20 +0800

    添加 axios 日志拦截

--- wiki-client/src/main.ts
+++ wiki-client/src/main.ts
@@ -8,4 +8,18 @@ import axios from "axios";
 
 axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 
+axios.interceptors.request.use(function (config) {
+    console.log("请求参数: ", config)
+    return config
+}, error => {
+    return Promise.reject(error)
+})
+
+axios.interceptors.response.use(function (res) {
+    console.log("返回结果: ", res)
+    return res
+}, error => {
+    return Promise.reject(error)
+})
+
 createApp(App).use(store).use(router).use(Antd).mount('#app')
